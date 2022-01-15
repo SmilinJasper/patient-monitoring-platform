b24d3a17676ad49eeb38da148b1db15999c27902
Smilin Jasper <d.sjd.sjj@gmail.com>
2022-01-15 23:12:23 +0530

    fix-title-of-doctor-login

--- doctor_login.php
+++ doctor_login.php
@@ -92,7 +92,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 
 <head>
-    <title>Staff Login</title>
+    <title>Doctor Login</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
