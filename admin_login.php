bd5c723539c5e89e1b38784ddad7b8bce2615bea
Smilin Jasper <d.sjd.sjj@gmail.com>
2022-01-15 23:08:28 +0530

    fix-title-of-admin-login-page

--- admin_login.php
+++ admin_login.php
@@ -92,7 +92,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 
 <head>
-    <title>Student Login</title>
+    <title>Admin Login</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
