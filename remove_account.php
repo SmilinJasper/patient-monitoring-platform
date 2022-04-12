ab9f91c7a49786a85514a7e94b2e321410785579
SmilinJasper <smilin.jasper@yahoo.com>
2022-04-12 22:25:15 +0530

    add-exception-throwing-on-account-removal

--- remove_account.php
+++ remove_account.php
@@ -2,7 +2,7 @@
 
 //Get basic info for deletion
 $id = $_GET['id']; // $id is now defined
-$account_type = $GET['account_type'];
+$account_type = $_GET['account_type'];
 
 // Include database file
 include "database.php";
@@ -10,20 +10,37 @@ include "database.php";
 //Delete entry of answersheet from MySQL table
 removeAccount($conn, $account_type, $id);
 
-function removeAccount($conn, $account_type, $id){
+function removeAccount($conn, $account_type, $id)
+{
 
-    if($account_type == "doctor") {
-        mysqli_query($conn, "DELETE FROM doctor_credentials WHERE id='".$id."'");
-        header("Location: admin_dashboard.php");
+    if ($account_type == "doctor") {
+
+        $sql = "DELETE FROM doctor_credentials WHERE id='" . $id . "'";
+
+        if (mysqli_query($conn, $sql)) {
+            // Redirect to admin dashboard
+            header("Location: admin_dashboard.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        //Close connection
+        mysqli_close($conn);
+
         return;
+    } else {
+
+        $sql =  "DELETE FROM patient_credentials WHERE id='" . $id . "'";
+
+        if (mysqli_query($conn, $sql)) {
+            // Redirect to doctor dashboard
+            header("Location: doctor_dashboard.php");
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        //Close connection
+        mysqli_close($conn);
+        
     }
-
-    mysqli_query($conn, "DELETE FROM patient_credentials WHERE id='".$id."'");
-    header("Location: doctor_dashboard.php");
-
-    //Close connection
-    mysqli_close($conn);
-
 }
-
-?>
